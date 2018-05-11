62c01342dec7938920f2fb4f19c84375ade639cf
Razibul Ahmed <razibul.ahmed@gmail.com>
2018-05-11 21:05:53 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -23,9 +23,8 @@ def get_contestant_name(data, occupation)
   return contestant_name
 end
 
-def count_contestants_by_hometown(data, hometown)
-  # code here
-end
+
+
 
 def get_occupation(data, hometown)
   # code here
