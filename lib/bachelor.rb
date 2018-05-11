9c508977d756b48fccc18542d2cfe61e132f9731
Razibul Ahmed <razibul.ahmed@gmail.com>
2018-05-11 21:10:28 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -24,8 +24,7 @@ def get_contestant_name(data, occupation)
 end
 
 def count_contestants_by_hometown(data, hometown)
-
-  # code here
+   # code here
   counter = 0
   data.each do |season, contestants|
     contestants.each do |stats|
